003fdcfc2f7f865cf98cf44d98c78bc8608a4476
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2023-11-01 23:47:01 +0530

    this is day 3

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from './chai';
+
+function App() {
+  const username = 'chai aur code';
+
+  return (
+    <>
+      <Chai />
+      <h1>chai aur react {username}</h1>
+      <p>test para</p>
+    </>
+  );
+}
+
+export default App;
